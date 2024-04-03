e5a47e1e5815e48918f129f4afb15416a52bec81
ivanfedotov <ivan78990@gmail.com>
2024-04-03 13:11:01 +0300

    "recent changes"

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from tkinter import *
 import pandas as pd
 import random
 
-def random_pair():
-    df = pd.read_csv("./data/french_words.csv")
-    list_of_dicts = [{row[df.columns[0]]: row[df.columns[1]]} for row in df.to_dict(orient='records')]
-    random_dict = random.choice(list_of_dicts)
-    random_word = next(iter(random_dict))
-    canvas.delete(french_word)
-    canvas.create_text(400, 263, text=f"{random_word}", font=("Ariel", 60, "bold"), fill='black')
 
 BACKGROUND_COLOR = "#B1DDC6"
+df = pd.read_csv("./data/french_words.csv")
+to_learn = df.to_dict(orient="records")
+
+def random_pair():
+    random_dict = random.choice(to_learn)
+    random_word = random_dict["French"]
+    canvas.itemconfig(canvas_word, text=random_word)
+    canvas.itemconfig(canvas_title, text="French")
+
+
+
 window = Tk()
 window.title("Flash Cards")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
@@ -20,8 +24,8 @@ card_back = PhotoImage(file="./images/card_back.png")
 
 canvas = Canvas(width=800, height=526)
 canvas.create_image(400, 263, image=card_front)
-canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"), fill='black')
-french_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill='black')
+canvas_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"), fill='black')
+canvas_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"), fill='black')
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -32,5 +36,7 @@ right_button.grid(row=1, column=1)
 wrong_image = PhotoImage(file="./images/wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=random_pair)
 wrong_button.grid(row=1, column=0)
+
+random_pair()
 
 window.mainloop()
